74d624513be7f606f5d9f68e24f6644df8f0a5a8
Kevin Langat <langatkaykay@gmail.com>
2025-06-26 12:47:13 +0300

    Added task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customer( 
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(20),
+  email VARCHAR(50),
+  address VARCHAR(100)
+  );
+  
+INSERT INTO customer(customer_id,customer_name,email,address) 
+Values (1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.');
