8eda6d87c91c412b79202b4258a3babb74fa770e
Kevin Langat <langatkaykay@gmail.com>
2025-06-26 12:57:33 +0300

    Updated the file again

--- task_6.sql
+++ task_6.sql
@@ -6,8 +6,12 @@
 -- Inserts multiple rows into the customer table
 -- Database name is passed as argument to mysql command
 
+-- task_6.sql
+-- Inserts multiple rows into the customer table with exact address formatting
+-- Database name is passed as argument to mysql command
+
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
     (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
